906490edab298f9483121204a5008bba969966e7
Giulio <zank@outlook.it>
2019-10-12 16:22:34 +0200

    Add all hero components

---
+++ src/views/components/HeroWebsites.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="section-hero section-shaped my-0">
+    <div class="shape shape-style-1 shape-primary">
+      <span class="span-150"></span>
+      <span class="span-50"></span>
+      <span class="span-50"></span>
+      <span class="span-75"></span>
+      <span class="span-100"></span>
+      <span class="span-75"></span>
+      <span class="span-50"></span>
+      <span class="span-100"></span>
+      <span class="span-50"></span>
+      <span class="span-100"></span>
+    </div>
+    <div class="container shape-container d-flex align-items-center">
+      <div class="col px-0">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-lg-7 text-center pt-lg">
+            <img src="img/brand/white.png" style="width: 200px;" class="img-fluid" />
+            <p class="lead text-white mt-4 mb-5">
+              <mark>S</mark>enseless and
+              <mark>V</mark>ery
+              <mark>U</mark>seless
+              <mark>A</mark>pps &
+              <mark>G</mark>ames.
+            </p>
+          </div>
+        </div>
+        <div class="row align-items-center justify-content-around stars-and-coded">
+          <div class="col-sm-4">
+            <a
+              href="https://github.com/zk-g/svuag-website-vue"
+              target="_blank"
+              title="Support us on Github"
+            >
+              <img src="img/brand/github-white-slim.png" style="height: 22px; margin-top: -3px" />
+            </a>
+          </div>
+          <div class="col-sm-4 mt-4 mt-sm-0 text-right">
+            <span class="text-white alpha-7">
+              Coded by just
+              <mark>2 people</mark>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
